46d40b5b59fef587547ea86d15862b612ac4fab7
Jon Rogers <67245+devnullvoid@users.noreply.github.com>
2026-05-31 00:14:18 -0400

    fix: resolve remaining hyprland 0.55 and niri config errors
    - Remove all windowrule one-liners (format incompatible with 0.55)
    - Remove layerrule one-liners (same issue)
    - Create empty dms include files for niri (cursor.kdl, windowrules.kdl)

--- home/system/hyprland/default.nix
+++ home/system/hyprland/default.nix
@@ -157,41 +157,7 @@ in {
         focus_on_activate = true;
       };
 
-      windowrule = [
-        "float, tag:modal"
-        "pin, tag:modal"
-        "center, tag:modal"
-        # telegram media viewer
-        "float, title:^(Media viewer)$"
 
-        # Bitwarden extension
-        "float, title:^(.*Bitwarden Password Manager.*)$"
-
-        # gnome calculator
-        "float, class:^(org.gnome.Calculator)$"
-        "size 360 490, class:^(org.gnome.Calculator)$"
-
-        # make Firefox/Zen PiP window floating and sticky
-        "float, title:^(Picture-in-Picture)$"
-        "pin, title:^(Picture-in-Picture)$"
-
-        # idle inhibit while watching videos
-        "idleinhibit focus, class:^(mpv|.+exe|celluloid)$"
-        "idleinhibit focus, class:^(zen)$, title:^(.*YouTube.*)$"
-        "idleinhibit fullscreen, class:^(zen)$"
-
-        "dimaround, class:^(gcr-prompter)$"
-        "dimaround, class:^(xdg-desktop-portal-gtk)$"
-        "dimaround, class:^(polkit-gnome-authentication-agent-1)$"
-        "dimaround, class:^(zen)$, title:^(File Upload)$"
-
-        # fix xwayland apps
-        "rounding 0, xwayland:1"
-        "center, class:^(.*jetbrains.*)$, title:^(Confirm Exit|Open Project|win424|win201|splash)$"
-        "size 640 400, class:^(.*jetbrains.*)$, title:^(splash)$"
-      ];
-
-      layerrule = ["noanim, launcher" "noanim, ^ags-.*"];
 
       input = {
         kb_layout = keyboardLayout;
